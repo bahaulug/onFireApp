5bd4569726949ce774f1d3dd1a2c0171cbe0916b
bahaulug <bahaulg@gmail.com>
2014-03-22 20:32:05 +0200

    bengi

---
+++ SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  onFire
+//
+//  Created by Bengisu Şahin on 20.03.2014.
+//  Copyright (c) 2014 Berkin Sansal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *settings;
+
+@end
