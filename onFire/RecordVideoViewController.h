0dddb9df5919be3cfff2cf745671f20605ca683c
bahaulug <support.onfire@outlook.com>
2014-03-25 17:34:32 +0200

    Video çekme eklendi

---
+++ onFire/RecordVideoViewController.h
@@ -0,0 +1,20 @@
+//
+//  RecordVideoViewController.h
+//  onFire
+//
+//  Created by Baha Ulug on 25.03.2014.
+//  Copyright (c) 2014 Berkin Sansal. All rights reserved.
+//
+
+#import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/UTCoreTypes.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface RecordVideoViewController: UIViewController
+
+-(IBAction)recordAndPlay:(id)sender;
+-(BOOL)startCameraControllerFromViewController:(UIViewController*)controller
+                                 usingDelegate:(id )delegate;
+-(void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void*)contextInfo;
+
+@end
---
+++ onFire/RecordVideoViewController.m
@@ -0,0 +1,94 @@
+//
+//  RecordVideoViewController.m
+//  onFire
+//
+//  Created by Baha Ulug on 25.03.2014.
+//  Copyright (c) 2014 Berkin Sansal. All rights reserved.
+//
+
+#import "RecordVideoViewController.h"
+
+@interface RecordVideoViewController ()
+- (IBAction)RecordAndPlay:(id)sender;
+
+@end
+
+@implementation RecordVideoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)RecordAndPlay:(id)sender {
+    
+    [self startCameraControllerFromViewController:self usingDelegate:self];
+}
+
+//For Starting to camera and recording video
+
+-(BOOL)startCameraControllerFromViewController:(UIViewController*)controller
+                                 usingDelegate:(id )delegate {
+    // 1 - Validattions
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO)
+        || (delegate == nil)
+        || (controller == nil)) {
+        return NO;
+    }
+    // 2 - Get image picker
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    // Displays a control that allows the user to choose movie capture
+    cameraUI.mediaTypes = [[NSArray alloc] initWithObjects:(NSString *)kUTTypeMovie, nil];
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    cameraUI.delegate = delegate;
+    // 3 - Display image picker
+    [controller presentModalViewController: cameraUI animated: YES];
+    return YES;
+}
+
+//Saving video to the media gallery
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    [self dismissModalViewControllerAnimated:NO];
+    // Handle a movie capture
+    if (CFStringCompare ((__bridge_retained CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
+        NSString *moviePath = [[info objectForKey:UIImagePickerControllerMediaURL] path];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(moviePath)) {
+            UISaveVideoAtPathToSavedPhotosAlbum(moviePath, self,
+                                                @selector(video:didFinishSavingWithError:contextInfo:), nil);
+        }
+    }
+}
+
+-(void)video:(NSString*)videoPath didFinishSavingWithError:(NSError*)error contextInfo:(void*)contextInfo {
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+                                                       delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+@end
